52417d11c0401788e5263a6ed480f0a54d23857f
macn <macn@itu.dk>
2024-02-26 15:44:35 +0100

    added functionality

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -7,10 +7,51 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Contact Name:
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          placeholder="Contact Name"
+        />
+      </label>
+      <br />
+      <label>
+        Phone Number:
+        <input
+          type="tel"
+          name="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+          pattern="(\(+45\)|\+45|\(0[1-9]\)|0[1-9])?( ?-?[0-9]){6,9}"
+          placeholder="Phone number"
+        />
+      </label>
+      <br />
+      <label>
+        Email :
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          autoCapitalize="none"
+          spellCheck="false"
+          required
+          pattern="^(?![\.\-_])((?![\-\._][\-\._])[a-z0-9\-\._]){0,63}[a-z0-9]@(?![\-])((?!--)[a-z0-9\-]){0,63}[a-z0-9]\.(|((?![\-])((?!--)[a-z0-9\-]){0,63}[a-z0-9]\.))(|([a-z]{2,14}\.))[a-z]{2,14}$"
+          placeholder="example@domain.com"
+        />
+      </label>
+      <br />
+      <input type="submit" value="Submit" />
+    </form>
   );
 };
-
